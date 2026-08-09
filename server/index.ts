7c6023582a8b40df983212944551489f11247d77
bomi0320 <84120820+bomi0320@users.noreply.github.com>
2026-08-09 23:22:59 +0900

    feat: add API endpoint for album search; implement query validation and iTunes API integration

--- server/index.ts
+++ server/index.ts
@@ -13,6 +13,41 @@ app.get('/api/test', (_req, res) => {
   });
 });
 
+app.get('/api/albums', async (req, res) => {
+  const query = req.query.query;
+
+  if (typeof query !== 'string' || !query.trim()) {
+    return res.status(400).json({
+      message: '검색어를 입력해주세요.',
+    });
+  }
+
+  try {
+    const params = new URLSearchParams({
+      term: query, // 사용자가 입력한 검색어
+      country: 'KR', // 우선 kr로 설정
+      media: 'music', // 음악 검색
+      entity: 'album', // 앨범만 검색
+      limit: '10', // 검색 결과 최대 10개만 가져오기
+    });
+
+    // iTunes 서버에 요청
+    const response = await fetch(
+      `https://itunes.apple.com/search?${params.toString()}`,
+    );
+
+    const data = await response.json();
+
+    res.json(data);
+  } catch (error) {
+    console.log(error);
+
+    res.status(500).json({
+      message: 'iTunes API 요청에 실패했습니다.',
+    });
+  }
+});
+
 // 서버 켜기
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
