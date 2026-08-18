955755bda5205d973c1466dfe46bd17bc102b5eb
bomi0320 <84120820+bomi0320@users.noreply.github.com>
2026-08-18 23:54:26 +0900

    feat: 앨범 프레임의 구조 및 색상 개선, 새로운 프레임 추가

--- src/scene/AlbumFrame.tsx
+++ src/scene/AlbumFrame.tsx
@@ -18,22 +18,83 @@ export default function AlbumFrame({
 }: AlbumFrameProps) {
   const [isHovered, setIsHovered] = useState(false);
 
+  const frameColor = isSelected
+    ? '#D9A6B3'
+    : isHovered
+      ? '#CFAE98'
+      : '#B99B7D';
+
   return (
     <group
       position={position}
       rotation={rotation}
+      scale={isHovered || isSelected ? 1.04 : 1}
       onPointerOver={() => setIsHovered(true)}
       onPointerOut={() => setIsHovered(false)}
       onClick={onSelect}
     >
-      {/* Frame */}
+      {/* Back Panel */}
       <mesh
         castShadow
         receiveShadow
+        position={[0, 0, -0.04]}
       >
-        <boxGeometry args={[1.2, 1.2, 0.08]} />
+        <boxGeometry args={[1.2, 1.2, 0.06]} />
         <meshStandardMaterial
-          color={isSelected ? '#d88cff' : '#8b6a4e'}
+          color="#E8DED5"
+          roughness={0.9}
+        />
+      </mesh>
+
+      {/* Top Frame */}
+      <mesh
+        castShadow
+        receiveShadow
+        position={[0, 0.55, 0]}
+      >
+        <boxGeometry args={[1.2, 0.1, 0.08]} />
+        <meshStandardMaterial
+          color={frameColor}
+          roughness={0.75}
+        />
+      </mesh>
+
+      {/* Bottom Frame */}
+      <mesh
+        castShadow
+        receiveShadow
+        position={[0, -0.55, 0]}
+      >
+        <boxGeometry args={[1.2, 0.1, 0.08]} />
+        <meshStandardMaterial
+          color={frameColor}
+          roughness={0.75}
+        />
+      </mesh>
+
+      {/* Left Frame */}
+      <mesh
+        castShadow
+        receiveShadow
+        position={[-0.55, 0, 0]}
+      >
+        <boxGeometry args={[0.1, 1.2, 0.08]} />
+        <meshStandardMaterial
+          color={frameColor}
+          roughness={0.75}
+        />
+      </mesh>
+
+      {/* Right Frame */}
+      <mesh
+        castShadow
+        receiveShadow
+        position={[0.55, 0, 0]}
+      >
+        <boxGeometry args={[0.1, 1.2, 0.08]} />
+        <meshStandardMaterial
+          color={frameColor}
+          roughness={0.75}
         />
       </mesh>
 
@@ -44,8 +105,11 @@ export default function AlbumFrame({
         position={[0, 0, 0.045]}
         onClick={onSelect}
       >
-        <planeGeometry args={[1.08, 1.08]} />
-        <meshBasicMaterial color="#f8f8f8" />
+        <planeGeometry args={[1.05, 1.05]} />
+        <meshStandardMaterial
+          color="#F7F1EB"
+          roughness={1}
+        />
       </mesh>
 
       {/* Album Cover */}
@@ -65,7 +129,7 @@ function AlbumCover({ image }: { image: string }) {
     <mesh
       castShadow
       receiveShadow
-      position={[0, 0, 0.05]}
+      position={[0, 0, 0.055]}
     >
       <planeGeometry args={[0.92, 0.92]} />
       <meshBasicMaterial map={texture} />
